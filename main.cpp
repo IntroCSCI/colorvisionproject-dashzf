f11320901ca9b29f4cf78bf7fe303182e784408e
dashzf <70167027+dashzf@users.noreply.github.com>
2020-11-16 23:58:58 -0800

    v0.2

--- main.cpp
+++ main.cpp
@@ -4,102 +4,106 @@
 
 using namespace std;
 
+void filterPixel(Pixel &, int);
+void printFilterMenu();
+
 int main()
 {
-  Pixel pixIn, pixOut;
-  int type;
+  Pixel currentPixel; //var to store RGB values of current pixels
+  Bitmap imageInput;
+  string imagePath;
+  vector < vector <Pixel> > rawPixelValues; //holds raw pixel values
+  int filterType;
   int errCnt = 0;
 
   do{
-  cout << "Enter red value (0-255): ";
-  cin >> pixIn.red;
-  }
-  while( pixIn.red > 255 || pixIn.red < 0 ); //check to make sure it's within range
+    if( errCnt >= 1 ){ //check to see if this loop has been repeated
+      cout << "Please input a valid .bmp file." << endl;
+    }
+    else {
+      cout << "Please input an image (must be a square .bmp with 24 bit color depth, case sensitive): ";
+    }
+    getline(cin, imagePath);
+    cout << endl;
+    imageInput.open(imagePath);
 
-  do{
-  cout << "Enter green value (0-255): ";
-  cin >> pixIn.green;
+    errCnt++;
   }
-  while( pixIn.green > 255 || pixIn.green < 0 ); //check to make sure it's within range
+  while( ! imageInput.isImage() ); //checks to see if it's a valid image
 
-  do{
-  cout << "Enter blue value (0-255): ";
-  cin >> pixIn.blue;
-  }
-  while( pixIn.blue > 255 || pixIn.blue < 0 ); //check to make sure it's within range
+  errCnt = 0;
 
   do{
     if( errCnt >= 1 ){ //check to see if this loop has been repeated
       cout << "Please enter a number between 1 and 4." << endl;
     }
     else {
-      cout << "\nWhat kind of color blindness do you want? (enter number)" << endl;
-      cout << "1. Monochromaticity - total color blindness." << endl;
-      cout << "2. Deuteranomaly - lack of ability to see green" << endl;
-      cout << "3. Protanopia - complete lack of ability to see red." << endl;
-      cout << "4. Tritanopia - lack of ability to see blue." << endl;
+      printFilterMenu();
     }
-    cin >> type;
+    cin >> filterType;
     cout << endl;
 
     errCnt++;
   }
-  while( type > 4 || type < 1);
+  while( filterType > 4 || filterType < 1);
+  
+  rawPixelValues = imageInput.toPixelMatrix(); //copies all pixel values from the image into this vector
+  cout << "Processing...\n";
 
+  for(int y = 0; y < rawPixelValues.size(); y++ ) { //Y counter
+    for(int x = 0; x < rawPixelValues.size(); x++ ) { //X counter
+      currentPixel = rawPixelValues[x][y];  //copy pixel value at current X and Y into currentPixel
+      filterPixel(currentPixel, filterType); //filter currentPixel
+      rawPixelValues[x][y] = currentPixel; //replace values in the vector with the adjusted ones
+    }
+  }
 
-  if( type == 1 ){
-    //Do monochromaticity
-    pixOut.red = (pixIn.red + pixIn.blue + pixIn.green)/3; //get average of all 3 numbers
-    pixOut.green = pixOut.red;
-    pixOut.blue = pixOut.red;
+  imageInput.fromPixelMatrix(rawPixelValues); //copy values from vector into the bmp
+  imageInput.save("out.bmp"); //save bmp
 
-  }
-  else if( type == 2 ){
-    //do Deuteranomaly - green
-    pixOut.red = pixIn.red;
-    pixOut.green = sqrt(pixIn.green);
-    pixOut.blue = pixIn.blue;
-  }
-  else if( type == 3 ) {
-    //do Protanopia - red
-    pixOut.red = sqrt(pixIn.red);
-    pixOut.green = pixIn.green;
-    pixOut.blue = pixIn.blue;
-  }
-  else if( type == 4) {
-    //do tritanopia
-    pixOut.red = pixIn.red;
-    pixOut.green = pixIn.blue;
-    pixOut.blue = sqrt(pixIn.blue);
-  }
-  cout << pixOut.red << ", " << pixOut.green << ", " << pixOut.blue << endl;
-
+  cout << "Done! Saved as out.bmp\n";
 
   return 0;
 }
 
 
+void filterPixel(Pixel & pixIn, int filterNum){
+  if( filterNum == 1 ){
+    //Do monochromaticity
+    pixIn.red = (pixIn.red + pixIn.blue + pixIn.green)/3; //get average of all 3 numbers
+    pixIn.green = pixIn.red;
+    pixIn.blue = pixIn.red;
 
+  }
+  else if( filterNum == 2 ){
+    //do Deuteranomaly - green
+    pixIn.red = pixIn.red;
+    pixIn.green = 0;
+    pixIn.blue = pixIn.blue;
+  }
+  else if( filterNum == 3 ) {
+    //do Protanopia - red
+    pixIn.red = 0;
+    pixIn.green = pixIn.green;
+    pixIn.blue = pixIn.blue;
+  }
+  else if( filterNum == 4) {
+    //do tritanopia
+    pixIn.red = pixIn.red;
+    pixIn.green = pixIn.blue;
+    pixIn.blue = 0;
+  }
+ // cout << pixIn.red << ", " << pixIn.green << ", " << pixIn.blue << endl;
 
-/*
-+@##########M/             :@#########@/
- ##############$;H#######@;+#############
- ###############M########################
- ##############X,-/++/+%+/,%#############
- ############M$:           -X############
- ##########H;.      ,--.     =X##########
- :X######M;     -$H@M##MH%:    :H#######@
-   =%#M+=,   ,+@#######M###H:    -=/M#%
-   %M##@+   .X##$, ./+- ./###;    +M##%
-   %####M.  /###=         @##M.   X###%
-   %####M.  ;M##H:.     =$###X.   $###%
-   %####@.   /####M$-./@#####:    %###%
-   %H#M/,     /H###########@:     ./M#%
-  ;$H##@@H:    .;$HM#MMMH$;,   ./H@M##M$=
- X#########%.      ..,,.     .;@#########
- ###########H+:.           ./@###########
- ##############/ ./%%%%+/.-M#############
- ##############H$@#######@@##############
- ##############X%########M$M#############
- +M##########H:            .$##########X=
- */
+  return;
+}
+
+void printFilterMenu(){
+  cout << "\nWhat kind of color blindness do you want? (enter number)" << endl;
+  cout << "1. Monochromaticity - total color blindness." << endl;
+  cout << "2. Deuteranomaly - lack of ability to see green" << endl;
+  cout << "3. Protanopia - complete lack of ability to see red." << endl;
+  cout << "4. Tritanopia - lack of ability to see blue." << endl;
+
+  return;
+}
